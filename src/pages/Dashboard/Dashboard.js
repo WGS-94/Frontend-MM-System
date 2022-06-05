b6621110c1c7cb4fddb76eb142f4fe64d5c31a21
Wilson Dos Santos <wilson-senx@hotmail.com>
2022-06-04 22:11:12 -0300

    :fire: finishing the dashbord page

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import { useAuth } from "../../hooks/auth";
-//import api from '../../services/api';
+import api from '../../services/api';
 
-import logoImg from '../../img/logo.svg';
 import NavBar from '../../components/Header/Nav';
 
 import './Dashboard.css';
@@ -10,15 +9,20 @@ import './Dashboard.css';
 function Dashboard() {
 
   const { user } = useAuth();
-  //const [data, setData] = React.useState([]);
+  const [data, setData] = React.useState([]);
 
-  /*React.useEffect(() => {
+  React.useEffect(() => {
     async function loadData() {
-      const response = await api.get(`/users/${user._id}`);
+      const response = await api.get(`/machines`);
+
+      const allStatus = response.data.status;
+
+      console.log(allStatus)
+      
       setData(response.data);
     }
     loadData();
-  }, [user._id]);*/
+  }, []);
 
   return (
     <div>
@@ -29,54 +33,39 @@ function Dashboard() {
           <div className="heading">
             <h1 className="heading__title">Machine Monitoring System - MMS</h1>
           </div>
-          <div className="cards">
-            <div className="card card-1">
+
+            <div className="cards">
+            {data.map((machine) => (
+            <div  key={machine._id} className="card card-1">
               <div className="card__content">
                 <div className="card__image">
-                  <img src={logoImg} alt="logomark" />
+                  <img src={machine.thumbnail_url} alt="machine" />
                 </div>
                 <div className="card__about">
                   <div className="card__about__product">
-                    <h2 className="card__title"><b>Nome:</b> Lorem ipsum dolor</h2>
-                    <h2 className="card__title"><b>Parâmetros:</b> Velocida, Pressão</h2>
-                    <h2 className="card__title"><b>Descrição:</b> Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae, laborum. Officiis porro perferendis consequatur iusto mollitia autem dolorum officia excepturi, consectetur ipsa ipsum soluta. Aut nulla nihil nesciunt et cum!</h2>
+                    <h2 className="card__title"><b>Nome:</b> {machine.machineName}</h2>
+                    <h2 className="card__title"><b>Parâmetros:</b> {machine.parameter}</h2>
+                    <h2 className="card__title"><b>Fabricante:</b> {machine.manufacturer}</h2>
+                    <h2 className="card__title"><b>Descrição:</b> {machine.description}</h2>
+                    <div className="card__status">
+                      <b>Ativo: </b>
+                      <span className={ machine.status === true ? "card__title_green" : "card__title_red"}></span>
+                    </div>
                   </div>
                   <div className="card__about__btn">
                     <span></span>
                     <div className='card__about__btns'>
-                      <button>Editar</button>
-                      <button>Excluir</button>
+                      <button className='btn1'>Editar</button>
+                      <button className='btn2'>Excluir</button>
                     </div>
-                    
                   </div>
                 </div>
               </div>
             </div>
 
-            <div className="card card-1">
-              <div className="card__content">
-                <div className="card__image">
-                  <img src={logoImg} alt="logomark" />
-                </div>
-                <div className="card__about">
-                  <div className="card__about__product">
-                    <h2 className="card__title"><b>Nome:</b> Lorem ipsum dolor</h2>
-                    <h2 className="card__title"><b>Parâmetros:</b> Velocida, Pressão</h2>
-                    <h2 className="card__title"><b>Descrição:</b> Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae, laborum. Officiis porro perferendis consequatur iusto mollitia autem dolorum officia excepturi, consectetur ipsa ipsum soluta. Aut nulla nihil nesciunt et cum!</h2>
-                  </div>
-                  <div className="card__about__btn">
-                    <span></span>
-                    <div className='card__about__btns'>
-                      <button>Editar</button>
-                      <button>Excluir</button>
-                    </div>
-                    
-                  </div>
-                </div>
-              </div>
-            </div>
-
+          ))}
           </div>
+
         </div>
       </div>
     </div>
