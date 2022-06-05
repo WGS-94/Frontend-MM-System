54524f1ba50436538e8bcb985eca4a5b4815f6dd
Wilson Dos Santos <wilson-senx@hotmail.com>
2022-06-04 23:59:14 -0300

    :fire: register new machine

--- src/pages/Machine/AddNewMachine/index.js
+++ src/pages/Machine/AddNewMachine/index.js
@@ -1,9 +1,40 @@
 import React from 'react';
-import './style.css';
+import { Form, Input } from "@rocketseat/unform";
+import Header from '../../../components/Header/Nav';
+
+import '../AddNewMachine/style.css';
 
 function AddNewMachine() {
+
+  function handleSubmitNewMachine() {
+
+  }
+  
   return (
-    <div>AddNewMachine</div>
+    <>
+      <Header />
+      <div className='container__newMachine'>
+        <div className="heading">
+          <h1 className="heading__title">Cadastro de uma nova Máquina</h1>
+        </div>
+        <div className="content__newMachine">
+
+          <div className="content__newMachine__fields">
+            <Form onSubmit={handleSubmitNewMachine}>
+              <label htmlFor="">Nome da Máquina: </label>
+              <Input type="text" />
+              <label htmlFor="">Parâmetro</label>
+              <Input type="text" />
+              <label htmlFor="">Fabricante</label>
+              <Input type="text" />
+              <label htmlFor="">Descrição</label>
+              <textarea name="" id="" />
+            </Form>
+          </div>
+        </div>
+
+      </div>
+    </>
   )
 }
 
---
+++ src/pages/Machine/EditAMachine/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './style.css';
+
+function AddAMachine() {
+  return (
+    <div>AddAMachine</div>
+  )
+}
+
+export default AddAMachine;
